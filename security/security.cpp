b9c84c761adfacb71f1f59e203a7553fd7c59f8d
admin <53293248+kvetinkavole@users.noreply.github.com>
2020-11-01 19:40:35 +0100

    fix

--- security/security.cpp
+++ security/security.cpp
@@ -1,99 +1,59 @@
-﻿/*
-
-        Created by: KaeexS#9426
+/*
+        Created by: KaeexS#9426, fixed and edited by kvetinka#9031
         For learning.
         You can add some crypt methods.
         You can add some Anti Debug, MemoryBreakPointsCheck, Anti Dumper, etc...
-
-
 */
 #include <iostream>
 #include <string>
 #include <time.h>
-#include <stdio.h>
-#include <windows.h>
-using namespace std; //I  use it for simple code. When you not using that program can be hardest for reverse
 
-
-string RandomString(int len) //Random String for some generators etc..
+std::string RandomString(int len) //Random String for some generators etc..
 {
-    string str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    string newstr;
-    int pos;
+    std::string str = { "0123456789ABCDEFAHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" }, newstr{}; int pos;
     while (newstr.size() != len) {
         pos = ((rand() % (str.size() - 1)));
         newstr += str.substr(pos, 1);
     }
     return newstr;
 }
-
-int QuitMe()
-{
-    return 0;
-}
-
 int main()
 {
+    int frst_value = { 1 }, sec_value = { 2 };//Using for if/else check//Using for if/else check
+    char encryptor[9];/*number of bytes*/ int enc{ 0 }, preset{}; //starts byte
+    //bytes
+    encryptor[enc++] = 's'; encryptor[enc++] = 'e'; encryptor[enc++] = 'c';
+    encryptor[enc++] = 'u'; encryptor[enc++] = 'r';
+    encryptor[enc++] = 'i'; encryptor[enc++] = 't';
+    encryptor[enc++] = 'y'; encryptor[enc++] = '\n'; //bytes
 
-    int frst_value = { 1 };//Using for if/else check
-    int sec_value = { 2 };//Using for if/else check
-
-
-    //that can be crypted URL
-    char encryptor[9]; //number of bytes
-    int enc{ 0 }, preset{}; // starting byte
-
-    encryptor[enc++] = 's';
-    encryptor[enc++] = 'e';
-    encryptor[enc++] = 'c';
-    encryptor[enc++] = 'u';
-    encryptor[enc++] = 'r';
-    encryptor[enc++] = 'i';
-    encryptor[enc++] = 't';
-    encryptor[enc++] = 'y';
-    encryptor[enc++] = '\n';
-
-    string crypt = encryptor;//crypted string in BYTES (CAN'T BE VISIBLE IN CONSOLE AND REVERSE TOOLS)
+    std::string crypt = encryptor;//crypted string in BYTES (CAN'T BE VISIBLE IN CONSOLE AND REVERSE TOOLS)
     srand((unsigned)time(0));
     int RandNum;
     RandNum = (rand() % 6) + 1;
 
-
-    SetConsoleTitle((LPCWSTR)"Security");
     if (frst_value < sec_value)
     {
+        int selection{ 0 };
+        std::cout << "   Securtiy LEARN \n   Created by: KaeexS#9426\n\n\n";
+        std::cout << "   1. Random Number\n";
+        std::cout << "   2. Random String\n";
+        std::cout << "   3. Crypted String in bytes(Anti Bsembler by: kvetinka#9031)\n\n";
+        std::cout << "   Select: ";  std::cin >> selection;
 
-        int selection;
-        cout << "   Securtiy LEARN \n   Created by: KaeexS#9426" << endl;
-        cout << "\n";
-        cout << "\n";
-        cout << "   1. Random Number" << endl;
-        cout << "   2. Random String" << endl;
-        cout << "   3. Crypted String in bytes(Anti Bsembler by: kvetinka#9031)" << endl;
-        cout << "\n";
-        cout << "   Select: "; cin >> selection; cout << "\n";
-        if (selection == 1)
+        switch (selection)
         {
-            cout << "[*] " << RandNum << endl << endl;
-            system("pause");
-            return 1;
+        case 1:
+            std::cout << "[*] " << RandNum << std::endl << std::endl;
+            system("pause"); break;
+        case 2:
+            std::cout << "[*] " << RandomString(20) << std::endl << std::endl;
+            system("pause"); break;
+        case 3:
+            std::cout << "[*] " << crypt << std::endl << std::endl;
+            system("pause"); break;
+        default:
+            std::cout << "command not found\n"; system("pause"); break;
         }
-        else if (selection == 2)
-        {
-            cout << "[*] " << RandomString(20) << endl << endl;
-            system("pause");
-            return 1;
-        }
-        else if (selection == 3)
-        {
-            cout << "[*] " << crypt << endl << endl;
-            system("pause");
-            return 1;
-        }
-    }
-    else
-    {
-        QuitMe(); //Quit the program
     }
 }
-
